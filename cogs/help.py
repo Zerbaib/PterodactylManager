ef5e7a1b7392c46a08c847e1e4c0478c5167be2c
Hennetin Hugo <hugo.hennetin@proton.me>
2023-09-20 14:12:20 +0200

    Help command

---
+++ cogs/help.py
@@ -0,0 +1,28 @@
+import disnake
+from disnake.ext import commands
+
+
+class HelpCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="help", description="See all the commands of the bot and how to use them")
+    async def help(self, ctx):
+        embed = disnake.Embed(
+            title="List of commands",
+            description="Here you wan find all the commands of the bot and their us",
+            color=disnake.Color.green()
+        )
+
+        for command in self.bot.commands:
+            if not command.hidden:
+                embed.add_field(
+                    name=f"**{self.bot.prefix}{command.name}**",
+                    value=command.help or "```Unknown```",
+                    inline=False
+                )
+
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
